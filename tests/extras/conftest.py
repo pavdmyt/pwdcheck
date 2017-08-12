5695ef39fd8e0ce4ffe7666a57331df22efb67a5
pavdmyt <dmytrenko.pavel@gmail.com>
2017-08-12 17:35:47 +0300

    implement test_palindrome tests

---
+++ tests/extras/conftest.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+tests.extras.conftest
+~~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+from copy import deepcopy
+
+import pytest
+
+
+@pytest.fixture(scope='module')
+def base_policy():
+    return {"extras": {"palindrome": True,
+                       "in_dictionary": True,
+                       "in_blacklist": True,
+                       "in_history": True}}
+
+
+# For consistency with /tests/cxty
+@pytest.fixture(scope='module')
+def mixed_policy(base_policy):
+    p = deepcopy(base_policy)
+    return p
+
+
+@pytest.fixture(scope='module')
+def false_palindrome_policy(base_policy):
+    p = deepcopy(base_policy)
+    p["extras"]["palindrome"] = False
+    return p
---
+++ tests/extras/test_palindrome.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+"""
+tests.extras.test_palindrome
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Tests for palindrome check.
+"""
+
+import pytest
+
+from pwdcheck.extras import Extras
+from pwdcheck.exceptions import ExtrasCheckError
+
+
+def test_false_palindrome(false_palindrome_policy):
+    # "palindrome": false
+    res_dct = Extras("foobar", false_palindrome_policy).as_dict
+    assert res_dct.palindrome == {}
+
+
+def test_empty_err_msg_if_no_err(mixed_policy):
+    # "palindrome": true
+    res_dct = Extras("foobar", mixed_policy).as_dict  # not a palindrome
+    assert not res_dct.palindrome.err        # no :err
+    assert res_dct.palindrome.err_msg == ""  # empty :err_msg
+
+
+def test_none_exc_if_no_err(mixed_policy):
+    # "palindrome": true
+    res_dct = Extras("foobar", mixed_policy).as_dict  # not a palindrome
+    assert not res_dct.palindrome.err        # no :err
+    assert res_dct.palindrome.exc is None    # :exc is None
+
+
+@pytest.mark.parametrize("pwd, expected_out", [
+    # Empty (the empty string is a palindrome)
+    (
+        "",  # <- password
+        {'err': True,
+         'err_msg': 'password is a palindrome',
+         'exc': ExtrasCheckError('password is a palindrome',),
+         'param_name': 'palindrome',
+         'policy_param_name': 'palindrome'}
+    ),
+
+    # Single character (also a palindrome)
+    (
+        "1",  # <- password
+        {'err': True,
+         'err_msg': 'password is a palindrome',
+         'exc': ExtrasCheckError('password is a palindrome',),
+         'param_name': 'palindrome',
+         'policy_param_name': 'palindrome'}
+    ),
+
+    # Classic example (also a palindrome)
+    (
+        "racecar",  # <- password
+        {'err': True,
+         'err_msg': 'password is a palindrome',
+         'exc': ExtrasCheckError('password is a palindrome',),
+         'param_name': 'palindrome',
+         'policy_param_name': 'palindrome'}
+    ),
+
+    # Not a palindrome
+    (
+        "foo",  # <- password
+        {'err': False,
+         'err_msg': '',
+         'exc': None,
+         'param_name': 'palindrome',
+         'policy_param_name': 'palindrome'}
+    )
+])
+def test_palindrome(pwd, expected_out, mixed_policy):
+    res_dct = Extras(pwd, mixed_policy).as_dict
+
+    for key, val in expected_out.items():
+        if key == 'exc' and res_dct.palindrome['exc']:
+            assert isinstance(res_dct.palindrome[key], ExtrasCheckError)
+        else:
+            assert res_dct.palindrome[key] == val
