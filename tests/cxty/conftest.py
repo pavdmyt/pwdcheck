fea1ce5eab30ee7e6f73a4947841aaea50a3263e
pavdmyt <dmytrenko.pavel@gmail.com>
2017-09-04 17:39:32 +0300

    simplify cxty.conftest

--- tests/cxty/conftest.py
+++ tests/cxty/conftest.py
@@ -44,7 +44,7 @@ def compose_nonint(policy_param_name):
         "foo-value",
         "list-value",
     ])
-    def nonint_policies(request, base_policy):
+    def nonint_policy(request, base_policy):
         p = deepcopy(base_policy)
 
         if request.param == "true-value":
@@ -57,7 +57,7 @@ def compose_nonint(policy_param_name):
             p["complexity"][policy_param_name] = [1, 2, 3]
 
         return p
-    return nonint_policies
+    return nonint_policy
 
 
 nonint_minlen_policy = compose_nonint("minlen")
@@ -67,86 +67,33 @@ nonint_umin_policy = compose_nonint("umin")
 nonint_omin_policy = compose_nonint("omin")
 
 
-#
-# minlen
-#
-@pytest.fixture(scope='module')
-def zero_minlen_policy(base_policy):
-    p = deepcopy(base_policy)
-    p["complexity"]["minlen"] = 0
-    return p
+def compose_zero(policy_param_name):
+    @pytest.fixture(scope='module')
+    def zero_policy(base_policy):
+        p = deepcopy(base_policy)
+        p["complexity"][policy_param_name] = 0
+        return p
+    return zero_policy
 
 
-@pytest.fixture(scope='module')
-def none_minlen_policy(base_policy):
-    p = deepcopy(base_policy)
-    del p["complexity"]["minlen"]
-    return p
+zero_minlen_policy = compose_zero("minlen")
+zero_dmin_policy = compose_zero("dmin")
+zero_lmin_policy = compose_zero("lmin")
+zero_umin_policy = compose_zero("umin")
+zero_omin_policy = compose_zero("omin")
 
 
-#
-# dmin
-#
-@pytest.fixture(scope='module')
-def zero_dmin_policy(base_policy):
-    p = deepcopy(base_policy)
-    p["complexity"]["dmin"] = 0
-    return p
+def compose_none(policy_param_name):
+    @pytest.fixture(scope='module')
+    def none_policy(base_policy):
+        p = deepcopy(base_policy)
+        del p["complexity"][policy_param_name]
+        return p
+    return none_policy
 
 
-@pytest.fixture(scope='module')
-def none_dmin_policy(base_policy):
-    p = deepcopy(base_policy)
-    del p["complexity"]["dmin"]
-    return p
-
-
-#
-# uppercase
-#
-@pytest.fixture(scope='module')
-def zero_umin_policy(base_policy):
-    p = deepcopy(base_policy)
-    p["complexity"]["umin"] = 0
-    return p
-
-
-@pytest.fixture(scope='module')
-def none_umin_policy(base_policy):
-    p = deepcopy(base_policy)
-    del p["complexity"]["umin"]
-    return p
-
-
-#
-# lowercase
-#
-@pytest.fixture(scope='module')
-def zero_lmin_policy(base_policy):
-    p = deepcopy(base_policy)
-    p["complexity"]["lmin"] = 0
-    return p
-
-
-@pytest.fixture(scope='module')
-def none_lmin_policy(base_policy):
-    p = deepcopy(base_policy)
-    del p["complexity"]["lmin"]
-    return p
-
-
-#
-# special
-#
-@pytest.fixture(scope='module')
-def zero_omin_policy(base_policy):
-    p = deepcopy(base_policy)
-    p["complexity"]["omin"] = 0
-    return p
-
-
-@pytest.fixture(scope='module')
-def none_omin_policy(base_policy):
-    p = deepcopy(base_policy)
-    del p["complexity"]["omin"]
-    return p
+none_minlen_policy = compose_none("minlen")
+none_dmin_policy = compose_none("dmin")
+none_lmin_policy = compose_none("lmin")
+none_umin_policy = compose_none("umin")
+none_omin_policy = compose_none("omin")
